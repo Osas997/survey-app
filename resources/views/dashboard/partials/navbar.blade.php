cca5aa6963acc4122f4dbd07eb2b5b3e39212081
Nasrul Fahmi Ulumuddin <103549350+NasFahmi@users.noreply.github.com>
2023-09-20 15:10:47 +0700

    fix navbar

--- resources/views/dashboard/partials/navbar.blade.php
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -31,14 +31,14 @@
                   </button>
                </div>
                <div
-                  class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+                  class="z-50 hidden my-4 text-base list-none w-52 bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
                   id="dropdown-user">
                   <div class="px-4 py-3" role="none">
                      <p class="text-sm text-gray-900 dark:text-white" role="none">
-                        Neil Sims
+                        Username
                      </p>
                      <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                        neil.sims@flowbite.com
+                        {{auth('admin')->user()->username}}
                      </p>
                   </div>
                   <ul class="py-1" role="none">
@@ -46,16 +46,6 @@
                         <a href="#"
                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                            role="menuitem">Dashboard</a>
-                     </li>
-                     <li>
-                        <a href="#"
-                           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                           role="menuitem">Settings</a>
-                     </li>
-                     <li>
-                        <a href="#"
-                           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                           role="menuitem">Earnings</a>
                      </li>
                      <li>
                         <a href="#"
