22c271fc6e00af118530aaa79fac54d5fca27e88
Nasrul Fahmi Ulumuddin <103549350+NasFahmi@users.noreply.github.com>
2023-09-20 20:16:41 +0700

    add form post in signout navbar

--- resources/views/dashboard/partials/navbar.blade.php
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -48,9 +48,13 @@
                            role="menuitem">Dashboard</a>
                      </li>
                      <li>
-                        <a href="#"
-                           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                           role="menuitem">Sign out</a>
+                        <form action="" method="post">
+                           @csrf
+                           <button href="#"
+                           type="submit"
+                              class="block text-start w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                              role="menuitem">Sign out</button>
+                        </form>
                      </li>
                   </ul>
                </div>
