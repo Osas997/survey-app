893148748140da8441c62041aa2c2fac5938c9ec
Nasrul Fahmi Ulumuddin <103549350+NasFahmi@users.noreply.github.com>
2023-09-22 10:40:02 +0700

    fix beheviour

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.pages')
 @section('title','Survey App')
 @section('content')
-<div class="w-screen h-screen overflow-x-hidden ">
+<div class="w-screen h-screen overflow-x-hidden scroll-smooth">
     <div class=" ">
         {{-- nav --}}
         <nav class="bg-[#0090D4] w-full z-20 top-0 left-0  ">
@@ -160,10 +160,11 @@
         <section class="px-4" id="about">
             <div class="max-w-screen-xl  mx-auto p-4 flex justify-center items-center">
                 <div class="w-full p-4 text-center bg-[#0062CE] rounded-[32px] border border-gray-200 shadow sm:p-10
-                lg:w-5/6">
-                    <h5 class="mb-2 text-3xl font-bold text-white 
+                lg:w-5/6
+                lg:py-16">
+                    <h5 class="mb-4 text-3xl font-bold text-white 
                     md:text-xl">Join Our Survey</h5>
-                    <p class="mb-5 text-base text-gray-100 sm:text-lg
+                    <p class="mb-6 text-base text-gray-100 sm:text-lg
                     md:text-base ">"Tolak Perundungan: Bersama-sama Kita Hentikan Tindakan Merugikan Ini!"</p>
                     <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
                         <a href="#" class="w-1/2  bg-white hover:bg-slate-100  focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 
@@ -188,7 +189,7 @@
                 <p class="text-slate-100 text-base text-center mb-10
                 lg:text-lg">"Mari bersama-sama menciptakan lingkungan yang aman dan bebas dari perundungan."</p>
                 <p class=" text-slate-300 text-xs text-center
-                lg:text-base">© 2023 SurveyApp. All Rights Reserved. </p>
+                lg:text-sm">© 2023 SurveyApp. All Rights Reserved. </p>
             </div>
         </footer>
     </div>
