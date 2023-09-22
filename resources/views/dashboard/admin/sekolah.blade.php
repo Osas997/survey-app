3a814c5ba8e71eb93ef0df1e717038ca40f29f3d
Nasrul Fahmi Ulumuddin <103549350+NasFahmi@users.noreply.github.com>
2023-09-22 15:32:12 +0700

    fix sekolah

--- resources/views/dashboard/admin/sekolah.blade.php
+++ resources/views/dashboard/admin/sekolah.blade.php
@@ -13,7 +13,7 @@
             </div>
             <input type="search" id="search" name="search" @if (request('search')) value="{{ request('search') }}"
                 @endif
-                class="block w-[240px] md:w-[500px] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="block w-[240px] md:w-[360px] lg:w-[400px] p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Cari Sekolah">
             <button type="submit"
                 class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
@@ -56,7 +56,8 @@
                 <th class="font-medium w-22 sm:w-1/8 py-4 text-sm sm:text-base md:text-lg lg:text-xl">
                     <span class="sm:text-base md:text-lg">Status</span>
                 </th>
-                <th></th>
+                <th class="w-22 sm:w-1/6">
+                </th>
             </tr>
         </thead>
 
@@ -79,9 +80,9 @@
                     <span class="sm:text-sm md:text-md">{{ $sekolah->status }}</span>
                 </td>
                 <td class="font-normal text-xs py-4">
-                    <div class="flex justify-center items-center gap-2">
-                        <a href="" class="text-white duration-300 ease-in-out bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0">Guru</a>
-                        <a href="" class="text-white duration-300 ease-in-out bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0">Murid</a>
+                    <div class="flex justify-center items-center gap-1">
+                        <a href="" class="text-white duration-300 ease-in-out bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 w-20 text-center mr-3 md:mr-0">Guru</a>
+                        <a href="" class="text-white duration-300 ease-in-out bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 w-20 text-center mr-3 md:mr-0">Murid</a>
                     </div>
                 </td>
                 
