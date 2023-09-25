721c3bbf5d0905d756dc5f6254f12b267e23b2a1
Nasrul Fahmi Ulumuddin <103549350+NasFahmi@users.noreply.github.com>
2023-09-25 20:55:21 +0700

    input exel file upload

--- resources/views/dashboard/sekolah/murid.blade.php
+++ resources/views/dashboard/sekolah/murid.blade.php
@@ -55,8 +55,10 @@
     </a>
 </div>
 <div class="mt-4">
-    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file Exel</label>
-    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" name="murid_exel" type="file"
+    <form action="" method="post" enctype="multipart/form-data">
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file Exel</label>
+        <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" name="murid_exel" type="file"
+    </form>
 </div>
 <div class="mt-8 w-full  overflow-x-auto overflow-y-auto rounded-lg">
     @if ($daftarMurid->isNotEmpty())
