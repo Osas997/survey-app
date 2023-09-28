752e79d4ed031c477f909a8260419ec7ed0a1152
Osas997 <larasatyt83@gmail.com>
2023-09-28 21:47:08 +0700

    laporaan siswa

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,12 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Guru;
+use App\Models\Murid;
+use App\Models\Sekolah;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +26,5 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
     }
 }
--- resources/views/dashboard/admin/murid.blade.php
+++ resources/views/dashboard/admin/murid.blade.php
@@ -68,7 +68,8 @@
                     </span>
                 </td>
                 <td class="px-6 py-4 text-center">
-                    <a class="sm:text-sm md:text-md text-teal-400" href="{{ $murid->id }}">Lihat Survey Respon</a>
+                    <a class="sm:text-sm md:text-md text-teal-400" href="{{ route('laporan_murid',$murid->id) }}">Lihat
+                        Survey Respon</a>
                 </td>
             </tr>
             @endforeach
